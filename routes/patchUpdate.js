fa9812b8e9f5e11497bf4f06ee940e4da6794abe
joeljoby02 <joel.aikkarakudiyiljoby@slu.edu>
2026-04-06 20:46:11 -0500

    Checking github autofix

--- routes/patchUpdate.js
+++ routes/patchUpdate.js
@@ -5,10 +5,18 @@ const router = express.Router()
 import controller from '../db-controller.js'
 import rest from '../rest.js'
 import auth from '../auth/index.js'
+import rateLimit from 'express-rate-limit'
+
+const patchUpdateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 patch-update requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false,
+})
 
 router.route('/')
-    .patch(auth.checkJwt, controller.patchUpdate) 
-    .post(auth.checkJwt, (req, res, next) => {
+    .patch(auth.checkJwt, patchUpdateLimiter, controller.patchUpdate) 
+    .post(auth.checkJwt, patchUpdateLimiter, (req, res, next) => {
         if (rest.checkPatchOverrideSupport(req, res)) {
             controller.patchUpdate(req, res, next)
         }
